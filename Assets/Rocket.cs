b4697494ea312cc92015b523e76c6ebc084e6441
Kaleb Wasmuth <kalebwasmuth@gmail.com>
2020-03-23 03:08:01 -0400

    Sounds + movement additions/fixes

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,32 +4,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket:MonoBehaviour {
-
     Rigidbody rocketBody;
+    AudioSource audio;
+
+    private double pitchGoal = 0.5f;
+    private double deltaPitch = 0.05;
+
+    [SerializeField] float thrustMultiplier = 700f;
+    [SerializeField] float rotationMultiplier = 50f;
 
     // Start is called before the first frame update
     void Start() {
         rocketBody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update() {
         ProcessInput();
+        moveAudioPitch();
+    }
+
+    private void moveAudioPitch() {
+        if(Math.Abs(pitchGoal - audio.pitch) > deltaPitch) {
+            audio.pitch += + (float) (Math.Sign(pitchGoal - audio.pitch) * deltaPitch);
+        } else {
+            audio.pitch = (float) pitchGoal;
+        }
     }
 
     private void ProcessInput() {
         if(Input.GetKey(KeyCode.Space)) {
             // Thrusting
-            rocketBody.AddRelativeForce(Vector3.up);
+            rocketBody.AddRelativeForce(Vector3.up * thrustMultiplier * Time.deltaTime);
+            pitchGoal = 3f;
+        } else {
+            pitchGoal = 0.5f;
         }
 
+        rocketBody.freezeRotation = true;
         if(Input.GetKey(KeyCode.A)) {
             // Rotate left
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationMultiplier * Time.deltaTime);
         }
         else if(Input.GetKey(KeyCode.D)) {
             // Rotate right
-            transform.Rotate(Vector3.back);
+            transform.Rotate(Vector3.back * rotationMultiplier * Time.deltaTime);
         }
+        rocketBody.freezeRotation = false;
     }
 }
