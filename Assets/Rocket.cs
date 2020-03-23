2c5123f5aec45e7618812a6c8accece28ebdefe8
Kaleb Wasmuth <kalebwasmuth@gmail.com>
2020-03-23 01:23:02 -0400

    Basic controls

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket:MonoBehaviour {
+
+    Rigidbody rocketBody;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rocketBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        
+        ProcessInput();
+    }
+
+    private void ProcessInput() {
+        if(Input.GetKey(KeyCode.Space)) {
+            // Thrusting
+            rocketBody.AddRelativeForce(Vector3.up);
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            // Rotate left
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D)) {
+            // Rotate right
+            transform.Rotate(Vector3.back);
+        }
     }
 }
